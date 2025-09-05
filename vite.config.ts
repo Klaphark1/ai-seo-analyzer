970c8f3658de5e3b55af83104128cafac4988b53
Klaphark1 <joshua.einhaus@gmail.com>
2025-09-05 12:46:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       define: {
-        'process.env.API_KEY': JSON.stringify(env.API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyDw4nmwCUQDRq-Amrxek0fLgszQ9-T3b0Y),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyDw4nmwCUQDRq-Amrxek0fLgszQ9-T3b0Y)
       },
       resolve: {
         alias: {
